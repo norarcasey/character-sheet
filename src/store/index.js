257ea276748e9422ed9e876a7bf4bf29f5016dcf
jcmckay <jcmckay@users.noreply.github.com>
2018-05-24 19:19:30 -0700

    enable dev tools for redux with middleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga'
 import rootReducer from '../reducers'
 import rootSaga from '../sagas'
@@ -6,14 +6,13 @@ import { PreloadedState  } from './preloadedState'
 
 const sagaMiddleware = createSagaMiddleware()
 
-/* eslint-disable no-underscore-dangle */
-const store = createStore(
-  rootReducer,
-  PreloadedState,
-  applyMiddleware(sagaMiddleware),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
-/* eslint-enable */
+// /* eslint-disable no-underscore-dangle */
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// /* eslint-enable */
+
+const store = createStore(rootReducer, PreloadedState, composeEnhancers(
+  applyMiddleware(sagaMiddleware)
+));
 
 sagaMiddleware.run(rootSaga)
 
