509eabb7bdc0377747a8e75c48a38235229fbbe5
jcmckay <jcmckay@users.noreply.github.com>
2018-05-27 13:24:44 -0700

    added race ability bonus display

--- src/components/RaceDetails.js
+++ src/components/RaceDetails.js
@@ -10,6 +10,7 @@ class RaceDetails extends Component {
 
     return (
       <div className="details">
+        {getAbilityBonus(details.ability_bonuses, this.props.attributes)}
         <p><label>Size:</label>{details.size}</p>
         <p><label>Speed:</label>{details.speed}</p>
         { concatPropNames('Traits', details.traits) }
@@ -20,6 +21,15 @@ class RaceDetails extends Component {
       </div>
     )
   }
+}
+
+const getAbilityBonus = (bonusArray, attributes) => {
+
+  let results = bonusArray.map((value, index) => {
+      return value > 0 ? `${attributes[index].text} +${value}` : false
+  }).filter(value => value)
+
+  return <p><label>Ability Bonuses:</label><span>{results.join(', ')}</span></p>
 }
 
 const mapStateToProps = state => ({
