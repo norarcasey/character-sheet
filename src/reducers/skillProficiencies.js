b07fda3e3441e24b1cb41fa8e94cd0de8e20e5fd
Justin McKay <justin.mckay@gettyimages.com>
2018-06-13 08:51:15 -0700

    code clean up

--- src/reducers/skillProficiencies.js
+++ src/reducers/skillProficiencies.js
@@ -1,22 +1,23 @@
 const skillProficiencies = (state = [], action) => {
-    switch (action.type) {
-      case 'UPDATE_SKILL_PROFICIENCY':
-        let stateContainsProficiency = state.indexOf(action.name) > -1;
+  switch (action.type) {
+    case 'UPDATE_SKILL_PROFICIENCY':
+      let stateContainsProficiency = state.indexOf(action.name) > -1;
 
-        if(!stateContainsProficiency && action.isProficient) {
-            return [...state, action.name]
-        } else if(stateContainsProficiency && !action.isProficient) {
-            return state.filter(skill => skill !== action.name)
-        } else {
-            return state
-        }
+      if(!stateContainsProficiency && action.isProficient) {
+        return [...state, action.name];
+      } else if(stateContainsProficiency && !action.isProficient) {
+        return state.filter(skill => skill !== action.name);
+      } else {
+        return state;
+      }
 
-      case 'RESET_SKILL_PROFICIENCIES':
-        return state.filter(pro => action.proficiencies.map(pro => pro.name).indexOf(pro) !== -1)
+    case 'RESET_SKILL_PROFICIENCIES':
+      let previouslySelectedProficiencies = action.proficiencies.map(pro => pro.name);
+      return state.filter(pro => previouslySelectedProficiencies.indexOf(pro) !== -1);
 
-      default:
-        return state
-    }
+    default:
+      return state;
   }
+};
 
-  export default skillProficiencies
+export default skillProficiencies;
