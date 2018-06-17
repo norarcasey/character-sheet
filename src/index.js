99f5b54745b3835d8e88442324a9fe170daa8e60
jcmckay <jcmckay@users.noreply.github.com>
2018-06-17 16:28:01 -0700

    solution doesn't work with dev

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import 'babel-polyfill';
 import App from './components/App';
 import './assets/styles/main.scss';
 
